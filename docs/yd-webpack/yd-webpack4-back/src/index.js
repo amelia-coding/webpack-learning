c5aaabd51f2786c5d840a6fdbe22b14c25d6fa8a
du.j@trip.com <sweety_juaner@163.com>
2020-07-31 10:29:12 +0800

    添加commonjs的打包

--- docs/yd-webpack/yd-webpack4-back/src/index.js
+++ docs/yd-webpack/yd-webpack4-back/src/index.js
@@ -1,6 +1,8 @@
-import "./index.css";
-import { result } from "./data";
-import {join} from "lodash-es";
+import { join } from 'lodash-es';
+import { result } from './data';
+import './index.css';
+const a = require('./cc');
+console.log(a);
 // class Foo {
 //     qq(){
 //         console.log("我是一个类-用到的方法");
@@ -13,17 +15,17 @@ import {join} from "lodash-es";
 // instance.qq();
 // {
 //     const xx = "123";
-// } 
+// }
 if (false) {
-    console.log("我是Tree treeshaking")
+  console.log('我是Tree treeshaking');
 }
 function init(num) {
-    const _sin = Math.sin(num);
-    const _sin2 = _sin + 0.004;
-    const cos = Math.cos(_sin2);
-    const xx = Math.sqrt(cos, 2);
-    console.log(xx);
+  const _sin = Math.sin(num);
+  const _sin2 = _sin + 0.004;
+  const cos = Math.cos(_sin2);
+  const xx = Math.sqrt(cos, 2);
+  console.log(xx);
 }
-init(20)
+init(20);
 console.log(result);
-console.log(join(["a", "b"], "🍊"));
+console.log(join(['a', 'b'], '🍊'));
--- docs/yd-webpack/yd-webpack4-back/webpack.config.js
+++ docs/yd-webpack/yd-webpack4-back/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const PurifyCSSPlugin = require('purifycss-webpack');
 const TerserPlugin = require('terser-webpack-plugin');
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const smp = new SpeedMeasurePlugin();
 const threadLoader = require('thread-loader');
@@ -16,7 +16,7 @@ const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 const notifier = require('node-notifier');
 const setTitle = require('node-bash-title');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-  setTitle('🍻 这是生产配置');
+setTitle('🍻 这是生产配置');
 // threadLoader.warmup({
 //     workers: 3,
 // }, [
@@ -24,80 +24,81 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 //     'css-loader',
 // ]);
 module.exports = smp.wrap({
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'cache-loader',
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            esModule: true,
-                        },
-                    },
-                    'css-loader',
-                ],
-                'exclude': /node_modules/,
-                'include': path.resolve(__dirname, 'src')
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'cache-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
             },
+          },
+          'css-loader',
         ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin({
-            test: /\.js(\?.*)?$/i,
-            cache: true,
-            parallel: true,
-        })],
-    },
-    plugins: [
-        // new WebpackDeepScopeAnalysisPlugin(),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),
-        new HtmlWebpackPlugin({  // Also generate a test.html
-            filename: 'test.html',
-            template: 'src/index.html'
-        }),
-        new HardSourceWebpackPlugin(),
-        new PurifyCSSPlugin({
-            // Give paths to parse for rules. These should be absolute!
-            paths: glob.sync(path.join(__dirname, 'src/*.html')),
-        }),
-        new ProgressBarPlugin(),
-        new WebpackBuildNotifierPlugin({
-            title: "🌶 老袁的Webpack",
-            logo: path.resolve("./img/favicon.png"),
-            suppressSuccess: true
-        }),
-        new FriendlyErrorsWebpackPlugin({
-            compilationSuccessInfo: {
-                messages: ['You application is running here http://localhost:3000'],
-                notes: ['Some additionnal notes to be displayed unpon successful compilation']
-            },
-            onErrors: function (severity, errors) {
-                // console.log("🐻",errors)
-                if (severity !== 'error') {
-                    return;
-                }
-                const error = errors[0];
-                notifier.notify({
-                    title: "🌶 老袁的Webpack",
-                    message: severity + ': ' + error.name,
-                    subtitle: error.file || '',
-                    // icon: ICON
-                });
-            },
-            // should the console be cleared between each compilation?
-            // default is true
-            clearConsole: true,
-        }),
-        // new PrepackWebpackPlugin()
-        new BundleAnalyzerPlugin()
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+      },
     ],
+  },
+  optimization: {
+    // minimize: true,
+    // minimizer: [new TerserPlugin({
+    //     test: /\.js(\?.*)?$/i,
+    //     cache: true,
+    //     parallel: true,
+    // })],
+  },
+  plugins: [
+    // new WebpackDeepScopeAnalysisPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: 'test.html',
+      template: 'src/index.html',
+    }),
+    new HardSourceWebpackPlugin(),
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+    }),
+    new ProgressBarPlugin(),
+    new WebpackBuildNotifierPlugin({
+      title: '🌶 老袁的Webpack',
+      logo: path.resolve('./img/favicon.png'),
+      suppressSuccess: true,
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:3000'],
+        notes: ['Some additionnal notes to be displayed unpon successful compilation'],
+      },
+      onErrors: function (severity, errors) {
+        // console.log("🐻",errors)
+        if (severity !== 'error') {
+          return;
+        }
+        const error = errors[0];
+        notifier.notify({
+          title: '🌶 老袁的Webpack',
+          message: severity + ': ' + error.name,
+          subtitle: error.file || '',
+          // icon: ICON
+        });
+      },
+      // should the console be cleared between each compilation?
+      // default is true
+      clearConsole: true,
+    }),
+    // new PrepackWebpackPlugin()
+    new BundleAnalyzerPlugin(),
+  ],
 });
